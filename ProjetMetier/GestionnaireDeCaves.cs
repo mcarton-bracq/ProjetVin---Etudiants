ba10ad362880e02bf0729766e5e07be4ab87255c
Maxime Carton-bracq <maxime.carton-bracq@outlook.fr>
2020-10-16 09:51:51 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetMetier/GestionnaireDeCaves.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetMetier
+{
+    public class GestionnaireDeCaves
+    {
+        private Dictionary<string, List<Bouteille>> lesCaves;
+
+        public GestionnaireDeCaves()
+        {
+            LesCaves = new Dictionary<string, List<Bouteille>>();
+        }
+
+        public Dictionary<string, List<Bouteille>> LesCaves { get => lesCaves; set => lesCaves = value; }
+
+        public void AjouterCave(string unNomDeCave,List<Bouteille> lesBouteillesDeLaCave)
+        {
+            lesCaves.Add(unNomDeCave, lesBouteillesDeLaCave);
+        }
+
+        public int NbBouteilles(string unNomDeCave)
+        {
+            foreach(Bouteille b in lesCaves)
+            {
+                if(unNomDeCave == b.)
+            }
+            return 0;
+        }
+
+        public int NbBouteillesDeRouges(string unNomDeCave)
+        {
+            int nb = 0;
+            
+            return 0;
+        }
+
+        public double ValeurDeLaCave(string unNomDeCave)
+        {
+
+            return 0;
+        }
+    }
+}
